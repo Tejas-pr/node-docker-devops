cee4bfe8d4261461756b4e5a43b7cbe51ba547e5
Tejas <tejas.teju02@gmail.com>
2025-09-29 23:46:42 +0530

    basic node js app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hi there!!");
+});
+
+app.listen(3000, () => {
+    console.log("the app is running on the port 3000");
+});
